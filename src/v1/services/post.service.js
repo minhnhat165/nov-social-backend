bd82cd3165748b35d0ca5e86d7617e0cec9cd161
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-04-04 07:59:34 +0700

    Add caching functionality for posts and implement like and unlike post feature

--- src/v1/services/post.service.js
+++ src/v1/services/post.service.js
@@ -2,6 +2,7 @@ const Post = require('../models/Post');
 const Hashtag = require('../models/Hashtag');
 const createHttpError = require('http-errors');
 const { deleteImages } = require('./cloud.service');
+const redis = require('../databases/init.redis');
 const createPost = async (post, user) => {
 	const {
 		content,
@@ -14,6 +15,7 @@ const createPost = async (post, user) => {
 
 	const { _id } = user;
 	const allHashtags = await handleHashtags(hashtagNames);
+
 	const newPoll = await handlePoll(poll, user);
 
 	const newPost = new Post({
@@ -30,6 +32,8 @@ const createPost = async (post, user) => {
 		{ _id: { $in: allHashtags } },
 		{ $push: { posts: savedPost._id } },
 	);
+	await savedPost.populate('author', 'avatar name');
+	cachePost(savedPost);
 	return savedPost;
 };
 
@@ -83,14 +87,17 @@ const updatePost = async (postId, post, user) => {
 		deleteImages(photoIdsRemove);
 		dataUpdate.photos = photos;
 	}
-	if (poll) {
+	if (poll || poll === null) {
 		const newPoll = await handlePoll(poll, user);
 		dataUpdate.poll = newPoll;
 	}
+
 	if (mentions) dataUpdate.mentions = mentions;
 	const updatedPost = await Post.findByIdAndUpdate(postId, dataUpdate, {
 		new: true,
 	}).select(Object.keys(dataUpdate).join(' '));
+
+	updatePostCached(postId, updatedPost._doc);
 	return updatedPost;
 };
 
@@ -115,7 +122,67 @@ const deletePost = async (id, user) => {
 	}
 
 	await post.remove();
+	deletePostCached(post._id);
 	return true;
+};
+
+const likePost = async (postId, user) => {
+	const { _id } = user;
+	const userId = _id.toString();
+	let post = await getPostCached(postId);
+	if (!post) {
+		post = await Post.findById(postId).select('likes');
+	}
+
+	if (!post) throw new createHttpError(404, 'Post not found');
+
+	const { likes } = post;
+	const isLiked = likes.some((like) => like.toString() === userId);
+
+	if (isLiked) throw new createHttpError(400, 'Post already liked');
+
+	await Post.findByIdAndUpdate(
+		postId,
+		{
+			$push: { likes: userId },
+		},
+		{ new: true },
+	);
+
+	const newLikes = [...likes, userId];
+	updatePostCached(postId, {
+		likes: newLikes,
+		likesCount: newLikes.length,
+	});
+
+	return post;
+};
+
+const unlikePost = async (postId, user) => {
+	const { _id } = user;
+	const userId = _id.toString();
+	let post = await getPostCached(postId);
+	if (!post) {
+		post = await Post.findById(postId).select('likes');
+	}
+
+	if (!post) throw new createHttpError(404, 'Post not found');
+
+	const { likes } = post;
+	const isLiked = likes.some((like) => like.toString() === userId);
+
+	if (!isLiked) throw new createHttpError(400, 'Post not liked yet');
+
+	const newLikes = likes.filter((like) => like.toString() !== userId);
+
+	await Post.findByIdAndUpdate(postId, {
+		$pull: { likes: userId },
+	});
+	updatePostCached(postId, {
+		likes: newLikes,
+		likesCount: newLikes.length,
+	});
+	return post;
 };
 
 const handleHashtags = async (hashtagNames) => {
@@ -157,10 +224,127 @@ const handlePoll = async (poll, user) => {
 	return { ...poll, options: newOptions };
 };
 
+const getPostsByListId = async (listId) => {
+	// Get all post from redis cache
+	const postCached = await redis.mget(
+		listId.map((id) => `${CACHE_POST_PREFIX}:${id}`),
+	);
+
+	// Get all post id not in redis cache
+	const postIdsNotCached = listId.filter((id, index) => !postCached[index]);
+
+	// Get all post not in redis cache from database
+	let postNotCached =
+		postIdsNotCached.length <= 0
+			? []
+			: await getPostsByListIdFromDatabase(postIdsNotCached);
+
+	// Set all post not in redis cache to redis cache
+	cachePosts(postNotCached);
+
+	// Combine all post from redis cache and post from database
+	const posts = [];
+
+	for (let i = 0; i < listId.length; i++) {
+		if (postCached[i]) {
+			posts.push(JSON.parse(postCached[i]));
+			continue;
+		}
+		const postId = listId[i];
+		const post = postNotCached.find(
+			(post) => post._id.toString() === postId,
+		);
+		if (post) posts.push({ ...post._doc, likesCount: post.likes.length });
+	}
+	return posts;
+};
+
+const getPostsByListIdFromDatabase = async (listId) => {
+	const posts = await Post.find({ _id: { $in: listId } })
+		.populate('author', 'name avatar')
+		.select('-__v -updatedAt -blockedUsers -allowedUsers');
+	return posts;
+};
+
+const getAllUserPostIds = async (userId) => {
+	const posts = await Post.find({ author: userId }).select('_id');
+	return posts.map((post) => post._id);
+};
+
+const retrievePostsSendToClient = (posts, userId) => {
+	return posts.map((post) => {
+		return retrievePostSendToClient(post, userId);
+	});
+};
+
+const retrievePostSendToClient = (post, userId) => {
+	const { likes, ...newPost } = post;
+	newPost.isLiked = likes.some(
+		(like) => like.toString() === userId.toString(),
+	);
+	return newPost;
+};
+
+// Cache
+const CACHE_POST_PREFIX = 'post';
+
+const cachePost = async (post) => {
+	post._doc.likesCount = post.likes.length;
+	redis.set(
+		`${CACHE_POST_PREFIX}:${post._id}`,
+		JSON.stringify(post),
+		'EX',
+		60 * 60 * 24,
+	); // 1 day
+};
+
+const cachePosts = async (posts) => {
+	const pipeline = redis.pipeline();
+	posts.forEach((post) => {
+		post._doc.likesCount = post.likes.length;
+		pipeline.set(
+			`${CACHE_POST_PREFIX}:${post._id}`,
+			JSON.stringify(post),
+			'EX',
+			60 * 60 * 24,
+		); // 1 day
+	});
+	await pipeline.exec();
+};
+
+const getPostCached = async (postId) => {
+	const postCached = await redis.get(`${CACHE_POST_PREFIX}:${postId}`);
+	if (!postCached) return null;
+	return JSON.parse(postCached);
+};
+
+const deletePostCached = async (postId) => {
+	redis.del(`${CACHE_POST_PREFIX}:${postId}`);
+};
+
+const updatePostCached = async (postId, update) => {
+	const postCached = await redis.get(`${CACHE_POST_PREFIX}:${postId}`);
+	if (!postCached) return;
+	const post = JSON.parse(postCached);
+	const newPost = { ...post, ...update };
+	redis.set(
+		`${CACHE_POST_PREFIX}:${postId}`,
+		JSON.stringify(newPost),
+		'EX',
+		60 * 60 * 24,
+	); // 1 day
+};
+
 const postService = {
 	createPost,
 	deletePost,
 	updatePost,
+	getAllUserPostIds,
+	getPostsByListId,
+	retrievePostsSendToClient,
+	likePost,
+	unlikePost,
+	retrievePostSendToClient,
 };
 
 module.exports = postService;
