ea9883e476f36ceb3f10eb8d67056bf2e24fe3ce
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-04-04 07:59:22 +0700

    Add follow and unfollow functionality to user service, and cache user's post ids using Redis

--- src/v1/services/user.service.js
+++ src/v1/services/user.service.js
@@ -1,6 +1,8 @@
 const createHttpError = require('http-errors');
-const { model } = require('mongoose');
 const User = require('../models/User');
+const redis = require('../databases/init.redis');
+const Post = require('../models/Post');
+const timelineService = require('./timeline.service');
 
 const createUser = async (user) => {
 	// Check if user is already existed
@@ -38,7 +40,6 @@ const searchByEmail = async (email, limit) => {
 
 const searchByName = async (name, limit, options) => {
 	if (!name) return [];
-	console.log(options);
 	return await User.find({
 		...options,
 		name: { $regex: name, $options: 'i' },
@@ -47,11 +48,117 @@ const searchByName = async (name, limit, options) => {
 		.select('name avatar email');
 };
 
+const followUser = async (userId, followId) => {
+	if (userId.toString() === followId.toString()) {
+		throw createHttpError(400, 'You cannot follow yourself');
+	}
+
+	const user = await User.findById(userId).select('following');
+
+	if (user.following.includes(followId)) {
+		throw createHttpError(400, 'User already followed');
+	}
+
+	await Promise.all([
+		User.findByIdAndUpdate(userId, { $addToSet: { following: followId } }),
+		User.findByIdAndUpdate(followId, { $addToSet: { followers: userId } }),
+	]);
+
+	timelineService.addMultipleToTimeline(
+		userId,
+		await getUserPostIds(followId),
+	);
+};
+
+const unFollowUser = async (userId, followId) => {
+	const user = await User.findById(userId).select('following');
+	if (!user.following.includes(followId)) {
+		throw createHttpError(400, 'User is not followed');
+	}
+
+	await Promise.all([
+		User.findByIdAndUpdate(userId, { $pull: { following: followId } }),
+		User.findByIdAndUpdate(followId, { $pull: { followers: userId } }),
+	]);
+
+	timelineService.removeMultipleFromTimeline(
+		userId,
+		await getUserPostIds(followId),
+	);
+};
+
+const retrieveUserSendToClient = (user, userReqId) => {
+	const {
+		followers,
+		following,
+		profilePrivate,
+		linkedAccounts = null,
+		providerId = null,
+		_v,
+		updatedAt,
+		notificationsCount,
+		status,
+		...restUser
+	} = user;
+	return {
+		...restUser,
+		followingCount: following.length,
+		followersCount: followers.length,
+		isFollowed: followers.includes(userReqId),
+	};
+};
+
+const getUserPostIds = async (userId) => {
+	let postIds = await getUserPostIdsFromCache(userId);
+	if (!postIds) {
+		postIds = await getUserPostIdsFromDB(userId);
+		await redis.hsetobj(`user:${userId}`, 'postIds', postIds);
+	}
+	return postIds;
+};
+
+const getUserPostIdsFromDB = async (userId) => {
+	const posts = await Post.find({ author: userId }).select('_id');
+	return posts.map((post) => post._id);
+};
+
+// cache
+
+const USER_CACHE_PREFIX = 'user';
+
+const getUserPostIdsFromCache = async (userId) => {
+	const key = `${USER_CACHE_PREFIX}:${userId}`;
+	const postIds = await redis.hgetobj(key, 'postIds');
+	if (postIds) return postIds;
+	return null;
+};
+
+const deleteUserPostIdsFromCache = async (userId) => {
+	const key = `${USER_CACHE_PREFIX}:${userId}`;
+	await redis.hdel(key, 'postIds');
+};
+
+const addPostIdToUserCache = async (userId, postId) => {
+	const key = `${USER_CACHE_PREFIX}:${userId}`;
+	await redis.hpusharr(key, 'postIds', postId, true);
+};
+
+const removePostIdFromUserCache = async (userId, postId) => {
+	const key = `${USER_CACHE_PREFIX}:${userId}`;
+	await redis.hpullarr(key, 'postIds', postId, true);
+};
+
 const userService = {
 	createUser,
 	getUser,
 	searchByEmail,
 	searchByName,
+	followUser,
+	unFollowUser,
+	retrieveUserSendToClient,
+	deleteUserPostIdsFromCache,
+	addPostIdToUserCache,
+	removePostIdFromUserCache,
 };
 
 module.exports = userService;
